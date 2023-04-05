b3b53691d8d1e3890561cb1acb873fd0f8c7a080
Hưng Võ <108314498+hungvo2003vn@users.noreply.github.com>
2023-04-05 09:03:44 +0700

    Update Tokens.py

--- Tokens.py
+++ Tokens.py
@@ -1,5 +1,6 @@
-OpenAi = "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"
-bot_token = "5908743585:AAFtlPpGd9T-ZoY4irCifQZlO3rZFbhMsVw"
-my_url = "https://hungvo2003vn.pythonanywhere.com"
+OpenAi = "YOUR_OPENAI_TOKEN"
+bot_token = "YOUR_BOT_TOKEN"
+my_url = "YOUR_ENDPOINT"
 secret = "receive_message"
-setwebhook_url = 'https://api.telegram.org/bot5908743585:AAFtlPpGd9T-ZoY4irCifQZlO3rZFbhMsVw/setWebhook?url=https://hungvo2003vn.pythonanywhere.com/receive_message&allowed_updates=["callback_query","message"]'
+setwebhook_url = 'https://api.telegram.org/bot<YOUR_BOT_TOKEN>/setWebhook?url=<YOUR_ENDPOINT>/receive_message&allowed_updates=["message", "edited_message", "channel_post", "edited_channel_post", "callback_query"]'
+CONNECTION_STRING = "YOUR_MONGODB_CLUSTER_URL"
