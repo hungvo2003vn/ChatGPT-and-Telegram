e3618200d2b571dd63c53451401ee8a0a9e7bd87
Hưng Võ <108314498+hungvo2003vn@users.noreply.github.com>
2023-04-05 09:57:38 +0700

    Update Tokens.py

--- Tokens.py
+++ Tokens.py
@@ -1,6 +1,6 @@
 OpenAi = "YOUR_OPENAI_TOKEN"
 bot_token = "YOUR_BOT_TOKEN"
 my_url = "YOUR_ENDPOINT"
-secret = "receive_message"
-setwebhook_url = 'https://api.telegram.org/bot<YOUR_BOT_TOKEN>/setWebhook?url=<YOUR_ENDPOINT>/receive_message&allowed_updates=["message", "edited_message", "channel_post", "edited_channel_post", "callback_query"]'
+secret = "YOUR_SECRET_KEY"
+setwebhook_url = 'https://api.telegram.org/bot<YOUR_BOT_TOKEN>/setWebhook?url=<YOUR_ENDPOINT>/<YOUR_SECRET_KEY>&allowed_updates=["message", "edited_message", "channel_post", "edited_channel_post", "callback_query"]'
 CONNECTION_STRING = "YOUR_MONGODB_CLUSTER_URL"
